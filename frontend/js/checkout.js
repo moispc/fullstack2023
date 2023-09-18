b776779077c9f9f9fc1c499e0a50a86c23219ba9
cerveux <diegomartinguzman@hotmail.com.ar>
2023-09-18 03:05:32 -0300

    feat: checkout

---
+++ frontend/js/checkout.js
@@ -0,0 +1,110 @@
+/**
+ * Define a function to navigate betweens form steps.
+ * It accepts one parameter. That is - step number.
+ */
+const navigateToFormStep = (stepNumber) => {
+    /**
+     * Hide all form steps.
+     */
+    document.querySelectorAll(".form-step").forEach((formStepElement) => {
+        formStepElement.classList.add("d-none");
+    });
+    /**
+     * Mark all form steps as unfinished.
+     */
+    document.querySelectorAll(".form-stepper-list").forEach((formStepHeader) => {
+        formStepHeader.classList.add("form-stepper-unfinished");
+        formStepHeader.classList.remove("form-stepper-active", "form-stepper-completed");
+    });
+    /**
+     * Show the current form step (as passed to the function).
+     */
+    document.querySelector("#step-" + stepNumber).classList.remove("d-none");
+    /**
+     * Select the form step circle (progress bar).
+     */
+    const formStepCircle = document.querySelector('li[step="' + stepNumber + '"]');
+    /**
+     * Mark the current form step as active.
+     */
+    formStepCircle.classList.remove("form-stepper-unfinished", "form-stepper-completed");
+    formStepCircle.classList.add("form-stepper-active");
+    /**
+     * Loop through each form step circles.
+     * This loop will continue up to the current step number.
+     * Example: If the current step is 3,
+     * then the loop will perform operations for step 1 and 2.
+     */
+    for (let index = 0; index < stepNumber; index++) {
+        /**
+         * Select the form step circle (progress bar).
+         */
+        const formStepCircle = document.querySelector('li[step="' + index + '"]');
+        /**
+         * Check if the element exist. If yes, then proceed.
+         */
+        if (formStepCircle) {
+            /**
+             * Mark the form step as completed.
+             */
+            formStepCircle.classList.remove("form-stepper-unfinished", "form-stepper-active");
+            formStepCircle.classList.add("form-stepper-completed");
+        }
+    }
+};
+/**
+ * Select all form navigation buttons, and loop through them.
+ */
+document.querySelectorAll(".btn-navigate-form-step").forEach((formNavigationBtn) => {
+    /**
+     * Add a click event listener to the button.
+     */
+    formNavigationBtn.addEventListener("click", () => {
+        /**
+         * Get the value of the step.
+         */
+        const stepNumber = parseInt(formNavigationBtn.getAttribute("step_number"));
+        /**
+         * Call the function to navigate to the target form step.
+         */
+        navigateToFormStep(stepNumber);
+    });
+});
+
+
+const address = document.querySelector("#first-method")
+const store = document.querySelector("#second-method")
+
+const switchButton = ()=>{
+    address.classList.toggle("unclickable")
+    address.classList.toggle("clickable")
+    store.classList.toggle("unclickable")
+    store.classList.toggle("clickable")
+}
+
+ address.addEventListener("click", ()=>{
+    /* address.classList.toggle("") */
+    switchButton();
+})
+
+store.addEventListener("click", ()=>{
+    /* address.disabled
+    store.disabled = true; */
+    switchButton();
+}) 
+
+
+const purchaseBtn = document.querySelector(".submit-btn");
+
+purchaseBtn.addEventListener("submit", (event) => {
+    console.log("entro");
+    event.preventDefault()
+    window.location.href = './exito.html';
+
+});
+
+const prueba2 = (e)=> {
+    window.location.href = './exito.html';
+    console.log("entro");
+    e.preventDefault()
+}
