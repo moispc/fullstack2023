6cd5daa7fbeaefd6b9c2bd704c091e49e1c9bec0
cerveux <diegomartinguzman@hotmail.com.ar>
2023-10-21 16:13:14 -0300

    agregados datos de usuario a la factura

--- js/exito.js
+++ js/exito.js
@@ -2,12 +2,27 @@ import { Cart } from "./modelos/cart.js";
 
 const cart = new Cart();
 
+const user = JSON.parse(localStorage.getItem('user')) || '';
+
+console.log(user);
+
 const table = document.querySelector(".table-container");
 const subtotal = document.querySelector(".subtotal")
 const total = document.querySelector(".total")
 const imprimir = document.querySelector(".imprimir")
+const userNombre = document.querySelector(".user-nombre")
+const compraTotal = document.querySelector('.compra-total')
+const fechaPago = document.querySelector('.fecha-pago')
+const userMail = document.querySelector('.user-mail')
 
 let pagoTotal = 0
+const date = new Date().toLocaleDateString('en-GB');
+
+userNombre.innerText = user.nombre;
+userMail.innerText = user.mail
+
+fechaPago.innerText = date
+
 
 
 cart.listArr.forEach(item => {
@@ -18,6 +33,8 @@ cart.listArr.forEach(item => {
                           <td class="text-end px-0">$${item.total.toFixed(2)}</td>`
 
     table.appendChild(tableRow)
+
+    compraTotal.innerText = `$ ${pagoTotal.toFixed(2)}`
 
     subtotal.innerHTML = `$ ${pagoTotal.toFixed(2)}`
     total.innerHTML = `$ ${pagoTotal.toFixed(2)}`
